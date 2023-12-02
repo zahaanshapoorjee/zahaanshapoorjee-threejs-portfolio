2fc2c693702c81ab8e59fb22d60f4b54ed6d23af
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2023-12-02 08:16:27 +0530

    Phone updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Space from '@/nextcomponents/space'
 import Navbar from '@/nextcomponents/navbar'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,9 @@ export default function RootLayout({
   
   return (
     <html lang="en">
+      <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body className={`${inter.className} bg-[#000000] overflow-y-scroll overflow-x-hidden `}>
         <Space/>
         <Navbar/>
