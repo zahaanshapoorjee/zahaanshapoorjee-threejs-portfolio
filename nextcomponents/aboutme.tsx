e7d8bb31c94870af78aede3968d67c1137d01b0b
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2023-12-02 08:06:53 +0530

    Phone updates

--- nextcomponents/aboutme.tsx
+++ nextcomponents/aboutme.tsx
@@ -148,35 +148,27 @@ const CameraLogger = () => {
     <motion.div
   initial="hidden"
   animate="visible"
-  className="flex flex-col md:flex-row items-center justify-center px-20 mt-15 w-full z-[20] h-full "
+  className="flex flex-col mt-20 md:flex-row items-center justify-center px-20 w-full z-[20] h-full md:flex-row items-center justify-center px-20 mt-10 w-full z-[20] h-full "
 >
-      <div className='h-full w-full flex flex-col gap-5 justify center m-auto text-start'>
-        <motion.div id="" className='hidden md:Welcome-box py-[15px] px-[4px] border border-[#D4AF37] opacity-[0.9] align-center px-6'
+      <div className='mb-20 md:h-full w-full flex flex-col gap-5 justify center m-auto mt-0 text-start'>
+        <motion.div className='Welcome-box py-[15px] px-[4px] border border-[#D4AF37] opacity-[0.9] align-center px-6'
         variants={slideTop}>
           <BoltIcon className='text-[#D4AF37] mr-[10px] h-5 w-5'/>
           <h1 className='text-white text-[13px]'>Athlete / Developer / Pianist</h1>
         </motion.div>
         <div  className='mt-20 md:flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'>
-        <span className='text-4xl md:text-6xl text-transparent bg-clip-text bg-gradient-to-r from-gold3 via-gold1 to-gold2 typewriter-text gold-gradient-text' data-type='["Zahaan", "Footballer", "Developer"]' data-period="3000"></span>
+        <span className='text-5xl md:text-6xl text-transparent bg-clip-text bg-gradient-to-r from-gold3 via-gold1 to-gold2 typewriter-text gold-gradient-text' data-type='["Zahaan", "Footballer", "Developer"]' data-period="3000"></span>
         </div>
         <motion.p
-        variants={slideLeft(0.8)} className='text-xl md:text-lg text-white my-5 max-w-[600px]'>
+        variants={slideLeft(0.8)} className='text-lg md:text-lg text-white my-5 max-w-[600px]'>
           Hi! I'm Zahaan, a footballer, developer, and pianist. My interests include Cyber-Security, Piano, and Full-Stack Development.
         </motion.p>
         <motion.a onClick={scrollToSkills} variants={slideLeft(1)} className='py-2 border border-[#D4AF37] opacity-[0.9]  text-center text-white cursor-pointer rounded-lg max-w-[200px]'>
             Check out my skills!
         </motion.a>
       </div>
-      {/* <motion.div variants={slideRight(0.8)} className='w-full h-full flex justify-center items-center'> */}
-      {/* <Image
-          className='iconsimage'
-          src="/mainIconsdark.svg"
-          alt="work icons"
-          height={650}
-          width={650}
-        /> */}
-        <div className='w-half md:w-full h-full flex justify-center items-center rotate-180 aboutme'>
-          <div style={{ width: '700px', height: '750px', position: 'relative' }}>
+        <div className='w-full h-full mt-10 md:w-full h-full flex justify-center items-center rotate-180 aboutme'>
+          <div className="canvas-container">
             <Canvas id="aboutme" camera={{position: [-20, 0, -15]}}>
               <Suspense fallback={null}>
                 <GoldenParticleModel />
