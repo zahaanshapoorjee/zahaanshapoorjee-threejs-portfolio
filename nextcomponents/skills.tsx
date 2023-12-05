c54403244ef6cd8a2ba9f24c284e774030d7e53e
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2023-12-06 00:34:48 +0530

    Added 3d model for some more idk

--- nextcomponents/skills.tsx
+++ nextcomponents/skills.tsx
@@ -1,40 +1,90 @@
 "use client"
-
-import React, { useEffect, useRef, useState, RefObject } from 'react';
-import { motion, useAnimation } from 'framer-motion';
-import Image from 'next/image';
-import { useLoader, useFrame, Canvas } from '@react-three/fiber';
+import React from 'react';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import * as THREE from 'three';
-import { Group } from 'three';
+import { useLoader } from '@react-three/fiber';
+
+const CppModel = () => {
+  const gltf = useLoader(GLTFLoader, '/cpp.glb');
+  return <primitive object={gltf.scene} scale={1} />;
+};
+
+const JSModel = () => {
+  const gltf = useLoader(GLTFLoader, '/js.glb');
+  return <primitive object={gltf.scene} scale={22.5} />;
+};
+
+const PythonModel = () => {
+  const gltf = useLoader(GLTFLoader, '/python.glb');
+  return <primitive object={gltf.scene} scale={2} />;
+};
+
+const ReactModel = () =>{
+  const gltf =  useLoader(GLTFLoader, '/react.glb');
+  return <primitive object={gltf.scene} scale={1}/>
+}
+
+const MongoDBModel = () =>{
+  const gltf =  useLoader(GLTFLoader, '/3dmongodb.glb');
+  return <primitive object={gltf.scene} scale={1}/>
+}
+
+
+const CameraOrbit = () => {
+  const { camera } = useThree();
+  useFrame(({ clock }) => {
+    const elapsed = clock.getElapsedTime();
+    const radius = 5; // Radius of the camera's circular path
+    camera.position.x = Math.sin(elapsed*1.25) * radius;
+    camera.position.z = Math.cos(elapsed*1.25) * radius;
+    camera.lookAt(0, 0, 0); // Make the camera look at the center of the scene (or the object)
+  });
+  return null;
+};
 
 const Skills = () => {
-
-  const CppModel = () => {
-    const gltf = useLoader(GLTFLoader, '/cpp2.glb');
-    const modelRef = useRef<Group>(null); // Explicitly type the ref as Group
-  
-    useFrame(() => {
-      if (modelRef.current) {
-        modelRef.current.rotation.y += 0.01;
-        // Add other rotation lines if needed
-      }
-    });
-  
-    return (
-      <primitive object={gltf.scene} scale={1} ref={modelRef} />
-    );
-  };
-
   return (
     <div className="grid grid-cols-4 gap-5 gap-y-10 mb-6 w-4/5 justify-center items-center rounded-3xl border-white pt-10 pb-10 border">
-          <div className='h-full w-full flex flex-row items-center justify-center'>
-        <Canvas camera={{position: [4, 0, 0]}} style={{width:150, height:150}}>
+      <div className='h-full w-full flex flex-row items-center justify-center'>
+        <Canvas camera={{ position: [4, 0, 0] }} style={{ width: 150, height: 150 }}>
           <ambientLight intensity={1.5} />
           <directionalLight position={[0, 0, 5]} />
+          <CameraOrbit />
           <CppModel />
         </Canvas>
-        </div>
+      </div>
+      <div className='h-full w-full flex flex-row items-center justify-center'>
+        <Canvas camera={{ position: [4, 0, 0] }} style={{ width: 150, height: 150 }}>
+          <ambientLight intensity={1.5} />
+          <directionalLight position={[0, 0, 5]} />
+          <CameraOrbit />
+          <JSModel />
+        </Canvas>
+      </div>
+      <div className='h-full w-full flex flex-row items-center justify-center'>
+        <Canvas camera={{ position: [4, 0, 0] }} style={{ width: 150, height: 150 }}>
+          <ambientLight intensity={1.5} />
+          <directionalLight position={[0, 0, 5]} />
+          <CameraOrbit />
+          <PythonModel />
+        </Canvas>
+      </div>
+      <div className='h-full w-full flex flex-row items-center justify-center'>
+        <Canvas camera={{ position: [4, 0, 0] }} style={{ width: 150, height: 150 }}>
+          <ambientLight intensity={7.5} />
+          <directionalLight position={[0, 0, 5]} />
+          <CameraOrbit />
+          <ReactModel />
+        </Canvas>
+      </div>
+      <div className='h-full w-full flex flex-row items-center justify-center'>
+        <Canvas camera={{ position: [14, 0, 0] }} style={{ width: 150, height: 150 }}>
+          <ambientLight intensity={4.5} />
+          <directionalLight position={[0, 0, 5]} />
+          <CameraOrbit />
+          <MongoDBModel />
+        </Canvas>
+      </div>
     </div>
   );
 };
--- nextcomponents/skillssection.tsx
+++ nextcomponents/skillssection.tsx
@@ -7,7 +7,7 @@ const SkillSection = () => {
     className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden md:mt-40"
     style={{ transform: "scale(0.85)" }}
   >
-    <h2 className='text-transparent bg-clip-text gold-gradient-text mb-2 text-6xl skilltext'>Skills</h2>
+    <h2 className='text-5xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-gold3 via-gold1 to-gold2 typewriter-text gold-gradient-text'>Skills</h2>
     <Skills/>
     </section>
   )
