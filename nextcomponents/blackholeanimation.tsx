40ef877c1f7efb33316d6128f71dda884a3a9429
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2024-02-16 15:51:14 +0530

    LOL idk

--- nextcomponents/blackholeanimation.tsx
+++ nextcomponents/blackholeanimation.tsx
@@ -7,6 +7,7 @@ function Blackholeanimation() {
     <div className='relative flex flex-col h-full w-full'>
       <video
   autoPlay
+  preload='true'
   muted
   loop
   className='rotate-180 md:absolute md:top-[-380px] left-0 z-[1] w-full h-full object-cover filtered-video'
