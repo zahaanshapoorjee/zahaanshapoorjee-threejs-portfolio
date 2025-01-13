3576d8a3d6267d82c3f365f605ffe970f8f5bd71
zahaanshapoorjee <zahaan@shapoorjee.com>
2025-01-13 16:04:51 +0530

    removed projects section test

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="h-full w-full">
       <div className="flex flex-col gap-15">
         <Blackholeanimation />
-        <SkillSection/>
+        {/* <SkillSection/> */}
         <CyberSec/>
         {/* <Projects/> */}
       </div>
