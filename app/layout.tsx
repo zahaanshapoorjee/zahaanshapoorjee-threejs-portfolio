50d1c57f9e356c305b40f840989037d9bde16c90
zahaanshapoorjee <zahaan@shapoorjee.com>
2025-01-13 17:30:07 +0530

    lets see if typewriterworked

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <meta name="viewport" content="width=device-width,initial-scale=1"/>
       </Head>
       <body className={`${inter.className} bg-[#000000] overflow-y-scroll overflow-x-hidden `}>
-        {/* <Space/> */}
+        <Space/>
         <Navbar/>
         {children}</body>
     </html>
--- nextcomponents/aboutme.tsx
+++ nextcomponents/aboutme.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BoltIcon } from '@heroicons/react/24/solid';
+import Typewriter from 'typewriter-effect'; 
 import Image from "next/image";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { Canvas, useLoader, useThree, useFrame } from "@react-three/fiber";
@@ -9,77 +10,12 @@ import { Suspense } from "react";
 import GoldenParticleModel from './goldenparticlemodel';
 import { OrbitControls } from '@react-three/drei';
 import { Vector3 } from 'three';
-
+import "./aboutme.css"
 function AboutMe() {
-  const isMobile = typeof window !== "undefined" && window.innerWidth < 768;
-  useEffect(() => {
-    class TxtType {
-      toRotate: string[];
-      el: HTMLElement;
-      loopNum: number;
-      period: number;
-      txt: string;
-      isDeleting: boolean;
-    
-      constructor(el: HTMLElement, toRotate: string[], periodAttr: string | null) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(periodAttr ?? '2000', 10); 
-        this.txt = '';
-        this.isDeleting = false;
-        this.tick();
-      }
-    
-      tick() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-    
-        if (this.isDeleting) {
-          this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-          this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-    
-        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-    
-        var that = this;
-        var delta = this.isDeleting ? 50 : 200; 
-    
-        if (this.isDeleting && this.txt === '') {
-          this.isDeleting = false;
-          this.loopNum++;
-          delta = 500;
-        } else if (!this.isDeleting && this.txt === fullTxt) {
-          this.isDeleting = true;
-          delta = this.period;
-        }
-    
-        setTimeout(() => {
-          that.tick();
-        }, delta);
-      }
-    }
-    
-    var elements = document.getElementsByClassName('typewriter-text');
-    for (var i = 0; i < elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-type');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate && elements[i] instanceof HTMLElement) {
-        new TxtType(elements[i] as HTMLElement, JSON.parse(toRotate), period);
-      }
-    }
-
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewriter-text > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
-  }, []);
 
   const scrollToSkills = () => {
     smoothScroll('skills', 2000); 
   };
-  
 
   const smoothScroll = (elementId: string, duration: number) => {
     const targetElement = document.getElementById(elementId);
@@ -107,7 +43,6 @@ function AboutMe() {
   
     requestAnimationFrame(animation);
   };
-  
 
   return (
     <div className="flex text-center flex-col md:flex-row mt-20 items-center justify-center px-0 md:px-20 md:pr-0 w-full z-[20] h-full">
@@ -117,7 +52,16 @@ function AboutMe() {
           <h1 className='text-white text-[13px]'>Athlete / Developer / Pianist</h1>
         </div>
         <div className='mt-20 md:flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'>
-          <span className='text-4xl md:text-6xl text-transparent bg-clip-text bg-gradient-to-r from-gold3 via-gold1 to-gold2 typewriter-text gold-gradient-text' data-type='["Zahaan", "Footballer", "Developer"]' data-period="3000"></span>
+          <Typewriter className='text-4xl md:text-6xl text-transparent bg-clip-text bg-gradient-to-r from-gold3 via-gold1 to-gold2 typewriter-text gold-gradient-text'
+            options={{
+              strings: ['Zahaan', 'Footballer', 'Developer'], // Array of words to type
+              autoStart: true,
+              loop: true,
+              typingDelay: 100,  // Adjust typing speed
+              deleteSpeed: 50,   // Adjust delete speed
+              cursorColor: "#D4AF37" // Cursor color
+            }}
+          />
         </div>
         <p className='text-lg md:text-lg text-white my-5 max-w-[600px]'>
           Hi! I'm Zahaan, a footballer, developer, and pianist. My interests include Cyber-Security, Piano, and Full-Stack Development.
@@ -127,14 +71,14 @@ function AboutMe() {
         </a>
       </div>
       <div className='w-full h-full mt-10 md:w-full h-full flex justify-center items-center rotate-180 aboutme '>
-      {!isMobile && <div className="canvas-container">
-        <Canvas id="aboutme" camera={{position: [-20, 0, -15]}}>
-          <Suspense fallback={null}>
-            <GoldenParticleModel />
-            <OrbitControls />
-          </Suspense>
-        </Canvas>
-      </div>}
+        <div className="canvas-container">
+          <Canvas id="aboutme" camera={{position: [-20, 0, -15]}}>
+            <Suspense fallback={null}>
+              <GoldenParticleModel />
+              <OrbitControls />
+            </Suspense>
+          </Canvas>
+        </div>
       </div>
     </div>
   );
